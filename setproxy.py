41793d3d2b7edae43f9f43e5bced8fa8b326a2dd
ArvanWorkshop <arvanbishwas504@gmail.com>
2020-04-22 18:01:27 +0600

    update

--- setproxy.py
+++ setproxy.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
 
 
-myProxy = "116.206.47.54:8080"
+
+
+myProxy = "13.55.51.101:8080"
+
 ip, port = myProxy.split(":")
 driver = webdriver.FirefoxProfile()
 driver.set_preference('network.proxy.type', 1)
 driver.set_preference('network.proxy.socks', ip)
 driver.set_preference('network.proxy.socks_port', int(port))
 driver = webdriver.Firefox(driver)
+
 driver.get('https://whoer.net/')
